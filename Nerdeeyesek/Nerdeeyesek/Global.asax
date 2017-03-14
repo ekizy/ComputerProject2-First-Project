9ba504e0542d19ba5a93d2a7845b50f71dc4590a
ekizy <ekizy@itu.edu.tr>
2017-03-14 20:59:30 +0300

    First commit

---
+++ Nerdeeyesek/Nerdeeyesek/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Nerdeeyesek.Global" Language="C#" %>
---
+++ Nerdeeyesek/Nerdeeyesek/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Nerdeeyesek.Startup))]
+namespace Nerdeeyesek
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
